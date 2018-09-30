d9313901cf1f369fb734aa8c22f6fbf65f084f5a
zlikun <zlikun-dev@hotmail.com>
2018-09-30 16:45:36 +0800

    初始化工程

---
+++ src/main/java/com/zlikun/jee/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.zlikun.jee.service;
+
+/**
+ * @author zhanglikun
+ * @version 1.0
+ * @date 2018-09-17 17:58
+ */
+public interface HelloService {
+
+    String hello(String name);
+
+}
---
+++ src/main/java/com/zlikun/jee/service/impl/HelloServiceImpl.java
@@ -0,0 +1,14 @@
+package com.zlikun.jee.service.impl;
+
+import com.zlikun.jee.service.HelloService;
+
+/**
+ * @author zhanglikun
+ * @version 1.0
+ * @date 2018-09-17 17:59
+ */
+public class HelloServiceImpl implements HelloService {
+    public String hello(String name) {
+        return null;
+    }
+}
